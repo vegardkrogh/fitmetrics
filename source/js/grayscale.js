c64f98e5da6b3979c433473f374bd5b33b96e4ae
Vegard Krogh-Reinsnes <vegardkrr@gmail.com>
2015-02-20 14:21:35 +0100

    adjustments to copy

--- source/js/grayscale.js
+++ source/js/grayscale.js
@@ -38,7 +38,7 @@ function init() {
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 14,
+        zoom: 12,
 
         // The latitude and longitude to center the map (always required)
         center: new google.maps.LatLng(59.894288, 10.627734), // New York
